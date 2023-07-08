fccbe25dc5b7bf561b22bddc838efc6844974cda
AsumVictor <iamasum369@gmail.com>
2023-07-08 04:15:27 -0700

    Updated readme and test

--- Testing/likeCounter.test.js
+++ Testing/likeCounter.test.js
@@ -10,6 +10,28 @@ describe('likeCount', () => {
     jest.clearAllMocks();
   });
 
+  test('Should return 0 when no item on the page', async () => {
+    const mockLikeSpanCounter = {
+      innerHTML: '3',
+      textContent: '3',
+    };
+    const mockLoveBtn = {
+      classList: {
+        contains: jest.fn().mockReturnValue(true),
+        replace: jest.fn(),
+      },
+    };
+    selectElFromDom.mockReturnValueOnce(mockLikeSpanCounter).mockReturnValueOnce(mockLoveBtn);
+
+    await likeCount(null);
+
+    expect(selectElFromDom).toHaveBeenCalledWith('.like-counter123');
+    expect(selectElFromDom).toHaveBeenCalledWith('#love123');
+    expect(mockLoveBtn.classList.contains).toHaveBeenCalledWith('fa-solid');
+    expect(mockLoveBtn.classList.replace).toHaveBeenCalledWith('fa-solid', 'fa-regular');
+    expect(mockLikeSpanCounter.innerHTML).toBe(null);
+  });
+
   test('should decrease like count and update DOM when love button is already Liked by use for one session', async () => {
     const mockLikeSpanCounter = {
       innerHTML: '3',
@@ -23,13 +45,13 @@ describe('likeCount', () => {
     };
     selectElFromDom.mockReturnValueOnce(mockLikeSpanCounter).mockReturnValueOnce(mockLoveBtn);
 
-    await likeCount(null, '123');
+    await likeCount('123');
 
     expect(selectElFromDom).toHaveBeenCalledWith('.like-counter123');
     expect(selectElFromDom).toHaveBeenCalledWith('#love123');
     expect(mockLoveBtn.classList.contains).toHaveBeenCalledWith('fa-solid');
     expect(mockLoveBtn.classList.replace).toHaveBeenCalledWith('fa-solid', 'fa-regular');
-    expect(mockLikeSpanCounter.innerHTML).toBe('2');
+    expect(mockLikeSpanCounter.innerHTML).toBe(2);
   });
 
   test('should Post new like and increase like count when user has not like before', async () => {
@@ -47,7 +69,7 @@ describe('likeCount', () => {
     selectElFromDom.mockReturnValueOnce(mockLikeSpanCounter).mockReturnValueOnce(mockLoveBtn);
     addLike.mockResolvedValueOnce(true);
 
-    await likeCount(null, '123');
+    await likeCount('123');
 
     expect(selectElFromDom).toHaveBeenCalledWith('.like-counter123');
     expect(selectElFromDom).toHaveBeenCalledWith('#love123');
@@ -55,7 +77,7 @@ describe('likeCount', () => {
     expect(addLike).toHaveBeenCalledWith(123);
     expect(mockLoveBtn.classList.replace).toHaveBeenCalledWith('fa-regular', 'fa-solid');
     expect(mockLoveBtn.classList.add).toHaveBeenCalledWith('like-before');
-    expect(mockLikeSpanCounter.innerHTML).toBe('4');
+    expect(mockLikeSpanCounter.innerHTML).toBe(4);
   });
 
   test('should increase like count when love button has class like-before but should not POST', async () => {
@@ -71,12 +93,13 @@ describe('likeCount', () => {
     };
     selectElFromDom.mockReturnValueOnce(mockLikeSpanCounter).mockReturnValueOnce(mockLoveBtn);
 
-    await likeCount(null, '123');
+    await likeCount('123');
 
     expect(selectElFromDom).toHaveBeenCalledWith('.like-counter123');
     expect(selectElFromDom).toHaveBeenCalledWith('#love123');
-    expect(mockLoveBtn.classList.contains).toHaveBeenCalledWith('fa-solid');
-    expect(mockLoveBtn.classList.replace).toHaveBeenCalledWith('fa-regular', 'fa-solid');
-    expect(mockLikeSpanCounter.innerHTML).toBe('4');
+    expect(mockLoveBtn.classList.contains).toHaveBeenCalledWith('like-before');
+    expect(mockLikeSpanCounter.innerHTML).toBe("3");
   });
+
+
 });
